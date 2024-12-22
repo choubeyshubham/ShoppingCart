ccc92b419a942fd10d49b80a4069f1e02ebe427c
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-12-22 15:47:32 +0530

    01 commit

---
+++ src/test/java/in/choubeyshubham/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package in.choubeyshubham.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
