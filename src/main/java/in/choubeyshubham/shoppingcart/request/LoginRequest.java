bd75c28d6455434b5800f6c8f6f7a30f34fa323f
Shubham Choubey <shubhamchoubey96@gmail.com>
2025-02-25 06:01:25 +0530

    26 commit login request

---
+++ src/main/java/in/choubeyshubham/shoppingcart/request/LoginRequest.java
@@ -0,0 +1,56 @@
+package in.choubeyshubham.shoppingcart.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+
+
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof LoginRequest that)) return false;
+        return Objects.equals(email, that.email) && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
